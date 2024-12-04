c6d0461075f3f794cd03fa0700866a114992dc78
Maria J Kirkou <mariajamalaki@hotmail.com>
2024-12-04 13:19:43 +0100

    Metoder i Account. Går att ändra sen

--- src/Account.java
+++ src/Account.java
@@ -1,2 +1,34 @@
 public class Account {
+    private String id;
+    private double balance;
+
+    public Account(String id, double initialBalance) {
+        this.id = id;
+        this.balance = initialBalance;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+
+    public void deposit(double amount) { // Insättning
+    }
+
+
+    public void withdraw(double amount) {// Uttag
+    }
+
+    // Betala faktura
+    public void payBill(String ocr, double amount) {
+    }
+
+    @Override
+    public String toString() {
+        //return
+    }
 }
