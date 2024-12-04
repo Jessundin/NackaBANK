1be76526393a0a2503fd85847573d8e2bed76cce
Maria J Kirkou <mariajamalaki@hotmail.com>
2024-12-04 13:10:16 +0100

    getters n setters i costumer

--- src/Costumer.java
+++ src/Costumer.java
@@ -1,4 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Costumer extends Person {
-    //hej
+    private String name;
+    private int age;
+    private List<Account> accounts;
+
+    public Costumer(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.accounts = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Getter för kontolistan
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void addAccount(Account account) {
+        this.accounts.add(account);
+    }
+
+    public void removeAccount(Account account) {
+        this.accounts.remove(account);
+    }
+
+    //behövs en override string toString här
 
 }
