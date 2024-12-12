b8f52a5081b922064a4a07c8773b08c45a9f252e
Jessica Sundin <jessundin@hotmail.com>
2024-12-12 19:39:15 +0100

    Trying out Singleton pattern

--- src/Bank.java
+++ src/Bank.java
@@ -4,14 +4,29 @@ import java.util.List;
 public class Bank {
 
     private List <Person> person;
+    private static Bank instance;
 
-    public Bank() {
+    private Bank() {
         this.person = new ArrayList<>();
+    }
+
+    public static Bank getInstance() {
+        if (instance == null) {
+            instance = new Bank();
+        }
+        return instance;
+    }
+
+    public List<Person> getPerson() {
+        return person;
     }
 
     public static void main(String[] args) {
 
         while (true) {
+
+            Bank bank = Bank.getInstance();
+
             Customer customer = new Customer();
             customer.welcomePrompt();
             customer.getWelcomeInputChoice();
